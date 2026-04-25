ccdf1f7968441ec22a47f9c8fdc2ef25eaa8c817
YATHARTH-77 <yatharthm.2006@gmail.com>
2026-04-25 12:11:12 +0530

    changes

--- ignition/modules/MultiSigWallet.ts
+++ ignition/modules/MultiSigWallet.ts
@@ -1,17 +1,15 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("MultiSigWalletModule", (m) => {
-  // Define the exact MetaMask addresses of your owners.
+  // Your 3 REAL MetaMask addresses go here
   const OWNERS = [
-    "0xFcF8bE0f7B50268678231dB7eE38c7c3dB74cd3c",          // Owner 1 (Your Sepolia Deployer)
-    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", // Owner 2 (Dummy testnet address)
-    "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"  // Owner 3 (Dummy testnet address)
+    "0xFcF8bE0f7B50268678231dB7eE38c7c3dB74cd3c",// Owner 1 (You)
+    "0x5a6c9C9CE4EbBE059DE63CEEE4efCDBf3335F83e",// Owner 2
+    "0x8Df8389C942dc3fADBF586BF8CC7b04538aA13Bd" // Owner 3
   ];
 
-  // Set the M-of-N threshold
   const THRESHOLD = 2;
 
-  // Deploy the contract with these exact arguments
   const multiSig = m.contract("MultiSigWallet", [OWNERS, THRESHOLD]);
 
   return { multiSig };
